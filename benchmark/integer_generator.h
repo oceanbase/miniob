15d92d478c5dd3fa1b4b8e17f30a287d08af62fb
wangyunlai <hnwyllmm@126.com>
2023-05-24 14:13:37 +0800

    format benchmark codes

--- benchmark/integer_generator.h
+++ benchmark/integer_generator.h
@@ -16,16 +16,11 @@ See the Mulan PSL v2 for more details. */
 class IntegerGenerator
 {
 public:
-  IntegerGenerator(int min, int max)
-    : distrib_(min, max)
-  {}
+  IntegerGenerator(int min, int max) : distrib_(min, max) {}
 
-  int next()
-  {
-    return distrib_(rd_);
-  }
+  int next() { return distrib_(rd_); }
 
 private:
-  std::random_device rd_;
+  std::random_device              rd_;
   std::uniform_int_distribution<> distrib_;
 };
