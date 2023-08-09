f7b7c9321720065c5faceb0cd2d81e2c7d3b14ea
wangyunlai <hnwyllmm@126.com>
2023-08-09 09:26:43 +0800

    Static gcc (#228)
    ### What problem were solved in this pull request?
    
    Issue Number: close #222 
    
    Problem:
    gcc使用动态链接库，在复制observer到其它机器后，无法启动。
    
    ### What is changed and how it works?
    使用gcc静态链接库

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ TOPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 BUILD_SH=$TOPDIR/build.sh
 
-CMAKE_COMMAND="cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --log-level=WARNING"
+CMAKE_COMMAND="cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --log-level=STATUS"
 
 ALL_ARGS=("$@")
 BUILD_ARGS=()
