91caf91f5681d0ce5b131903d05bc75fc8e9a4e9
王运来 <hnwyllmm@126.com>
2023-08-14 15:27:26 +0800

    expression: support arithmetic

--- src/observer/sql/expr/expression.h
+++ src/observer/sql/expr/expression.h
@@ -16,6 +16,8 @@ See the Mulan PSL v2 for more details. */
 
 #include <string.h>
 #include <memory>
+#include <string>
+
 #include "storage/field/field.h"
 #include "sql/parser/value.h"
 #include "common/log/log.h"
@@ -40,6 +42,7 @@ enum class ExprType
   CAST,         ///< 需要做类型转换的表达式
   COMPARISON,   ///< 需要做比较的表达式
   CONJUNCTION,  ///< 多个表达式使用同一种关系(AND或OR)来联结
+  ARITHMETIC,   ///< 算术运算
 };
 
 /**
@@ -81,7 +84,11 @@ public:
    */
   virtual AttrType value_type() const = 0;
 
-  virtual std::string name() const { return ""; }
+  virtual std::string name() const { return name_; }
+  virtual void set_name(std::string name) { name_ = name; }
+
+private:
+  std::string  name_;
 };
 
 /**
@@ -306,4 +313,43 @@ public:
 private:
   Type conjunction_type_;
   std::vector<std::unique_ptr<Expression>> children_;
+};
+
+/**
+ * @brief 算术表达式
+ */
+class ArithmeticExpr : public Expression 
+{
+public:
+  enum class Type {
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    NEGATIVE,
+  };
+
+public:
+  ArithmeticExpr(Type type, std::unique_ptr<Expression> left, std::unique_ptr<Expression> right);
+  virtual ~ArithmeticExpr() = default;
+
+  ExprType type() const override { return ExprType::ARITHMETIC; }
+
+  AttrType value_type() const override;
+
+  RC get_value(const Tuple &tuple, Value &value) const override;
+  RC try_get_value(Value &value) const override;
+
+  Type arithmetic_type() const { return arithmetic_type_; }
+
+  std::unique_ptr<Expression> &left() { return left_; }
+  std::unique_ptr<Expression> &right() { return right_; }
+
+private:
+  RC calc_value(const Value &left_value, const Value &right_value, Value &value) const;
+  
+private:
+  Type arithmetic_type_;
+  std::unique_ptr<Expression> left_;
+  std::unique_ptr<Expression> right_;
 };
