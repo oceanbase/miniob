12d41617da9d16c60272e99989b7b3c1161bc3da
王运来 <hnwyllmm@126.com>
2023-09-01 17:23:08 +0800

    docker: security

--- docker/bin/starter.sh
+++ docker/bin/starter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ls -lld $PWD/*starter-* | awk '{print $9;}' | xargs -L 1 bash -c
+ls -lld $HOME/docker/bin/*starter-* | awk '{print $9;}' | xargs -L 1 bash -c
 echo 'starter scripts run successfully!'
 tail -f /dev/null
