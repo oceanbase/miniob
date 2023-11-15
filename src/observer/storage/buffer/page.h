16ac419cb662aa09ac0be3ddcf616987117d1a49
王运来 <hnwyllmm@126.com>
2023-11-15 16:14:26 +0800

    clang-format

--- src/observer/storage/buffer/page.h
+++ src/observer/storage/buffer/page.h
@@ -14,16 +14,16 @@ See the Mulan PSL v2 for more details. */
 
 #pragma once
 
-#include <stdint.h>
 #include "common/types.h"
+#include <stdint.h>
 
 using TrxID = int32_t;
 
 static constexpr int BP_INVALID_PAGE_NUM = -1;
 
-static constexpr PageNum BP_HEADER_PAGE   = 0;
+static constexpr PageNum BP_HEADER_PAGE = 0;
 
-static constexpr const int BP_PAGE_SIZE = (1 << 13);
+static constexpr const int BP_PAGE_SIZE      = (1 << 13);
 static constexpr const int BP_PAGE_DATA_SIZE = (BP_PAGE_SIZE - sizeof(PageNum) - sizeof(LSN));
 
 /**
@@ -34,5 +34,5 @@ struct Page
 {
   PageNum page_num;
   LSN     lsn;
-  char data[BP_PAGE_DATA_SIZE];
+  char    data[BP_PAGE_DATA_SIZE];
 };
