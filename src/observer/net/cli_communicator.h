18b9107cb83bde3205b4f319bb9f5fb13002d3a2
王运来 <hnwyllmm@126.com>
2024-01-15 16:00:46 +0800

    clang-format

--- src/observer/net/cli_communicator.h
+++ src/observer/net/cli_communicator.h
@@ -35,6 +35,6 @@ public:
   bool exit() const { return exit_; }
 
 private:
+  bool exit_ = false;  ///< 是否需要退出
   int write_fd_ = -1;  ///< 与使用远程通讯模式不同，如果读数据使用标准输入，那么输出应该是标准输出
-  bool exit_ = false;
 };
