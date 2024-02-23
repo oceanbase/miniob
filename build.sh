57412f6e04cbe09de8c52dea86cf0b6a9f79b1b8
colommar <yfxx_weiyx@163.com>
2024-02-23 21:59:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ CMAKE_COMMAND="cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --log-level=STATUS"
 
 ALL_ARGS=("$@")
 BUILD_ARGS=()
-MAKE_ARGS=(-j $CPU_CORES)
+MAKE_ARGS=()
 MAKE=make
 
 echo "$0 ${ALL_ARGS[@]}"
