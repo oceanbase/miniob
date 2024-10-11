4b072c9abe41ffbc17c35a29aef41a2319a969cd
sisabyss <xiwei_tao@outlook.com>
2024-10-11 14:22:17 +0800

    fix memory leak in RowTuple::set_schema

--- src/observer/sql/expr/tuple.h
+++ src/observer/sql/expr/tuple.h
@@ -178,6 +178,9 @@ public:
     table_ = table;
     // fix:join当中会多次调用右表的open,open当中会调用set_scheme，从而导致tuple当中会存储
     // 很多无意义的field和value，因此需要先clear掉
+    for (FieldExpr *spec : speces_) {
+      delete spec;
+    }
     this->speces_.clear();
     this->speces_.reserve(fields->size());
     for (const FieldMeta &field : *fields) {
