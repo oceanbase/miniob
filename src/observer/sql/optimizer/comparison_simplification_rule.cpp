1fa1b9e1662cd970ec7741d1c715714a43f61fa3
nauta <yuhang481@gmail.com>
2025-02-07 14:27:48 +0800

    ci test

--- src/observer/sql/optimizer/comparison_simplification_rule.cpp
+++ src/observer/sql/optimizer/comparison_simplification_rule.cpp
@@ -12,6 +12,7 @@ See the Mulan PSL v2 for more details. */
 // Created by Wangyunlai on 2022/12/13.
 //
 
+
 #include "sql/optimizer/comparison_simplification_rule.h"
 #include "common/log/log.h"
 #include "sql/expr/expression.h"
