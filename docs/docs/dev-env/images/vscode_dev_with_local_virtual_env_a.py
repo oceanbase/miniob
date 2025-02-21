a91b31df36607d1b28d0f9791dff054b1aa075f5
XuPing <haibarapink@gmail.com>
2025-02-21 16:04:54 +0800

    删除无关文件

--- docs/docs/dev-env/images/vscode_dev_with_local_virtual_env_a.py
+++
@@ -1,8 +0,0 @@
-import os
-
-prefix = "vscode_dev_with_local_virtual_env_"
-
-for filename in os.listdir("."):
-    if os.path.isfile(filename):
-        new_name = prefix + filename
-        os.rename(filename, new_name)
