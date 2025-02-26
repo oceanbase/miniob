26850c1a28e85fa249f11360a0cb0acff9163091
linknight423 <52560052+linknight423@users.noreply.github.com>
2025-02-26 20:10:02 +0800

    Add files via upload

--- src/observer/sql/executor/command_executor.cpp
+++ src/observer/sql/executor/command_executor.cpp
@@ -15,6 +15,7 @@ See the Mulan PSL v2 for more details. */
 #include "sql/executor/command_executor.h"
 #include "common/log/log.h"
 #include "event/sql_event.h"
+#include "sql/executor/drop_table_executor.h"
 #include "sql/executor/create_index_executor.h"
 #include "sql/executor/create_table_executor.h"
 #include "sql/executor/desc_table_executor.h"
@@ -42,6 +43,11 @@ RC CommandExecutor::execute(SQLStageEvent *sql_event)
       rc = executor.execute(sql_event);
     } break;
 
+    case StmtType::DROP_TABLE: {
+      DropTableExecutor executor;
+      rc = executor.execute(sql_event);
+    } break;
+    
     case StmtType::DESC_TABLE: {
       DescTableExecutor executor;
       rc = executor.execute(sql_event);
