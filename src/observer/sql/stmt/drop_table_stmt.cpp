7f25f05189cfe64a6eeb57845731aceec7cf8e06
linknight423 <52560052+linknight423@users.noreply.github.com>
2025-02-26 20:11:33 +0800

    Add files via upload

---
+++ src/observer/sql/stmt/drop_table_stmt.cpp
@@ -0,0 +1,25 @@
+/* Copyright (c) 2021 OceanBase and/or its affiliates. All rights reserved.
+miniob is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details. */
+
+//
+// Created by Wangyunlai on 2023/6/13.
+//
+
+#include "common/log/log.h"
+#include "common/types.h"
+#include "sql/stmt/drop_table_stmt.h"
+#include "event/sql_debug.h"
+
+RC DropTableStmt::create(Db *db, const DropTableSqlNode &drop_table, Stmt *&stmt)
+{
+  stmt = new DropTableStmt(drop_table.relation_name);
+  return RC::SUCCESS;
+}
+
