1c28aca2d91fee30430d58f5a85bca07ec3aa017
wz 魏 <1265439271@qq.com>
2025-04-05 13:14:49 +0000

    date1

--- src/observer/common/type/date_type.h
+++ src/observer/common/type/date_type.h
@@ -1,18 +1,11 @@
-/* Copyright (c) 2021 OceanBase and/or its affiliates. All rights reserved.
-miniob is licensed under Mulan PSL v2.
-You can use this software according to the terms and conditions of the Mulan PSL v2.
-You may obtain a copy of Mulan PSL v2 at:
-         http://license.coscl.org.cn/MulanPSL2
-THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-See the Mulan PSL v2 for more details. */
-
 #pragma once
 
 #include "common/type/data_type.h"
 
-
+/**
+ * @brief   日期类型
+ * @ingroup DataType
+ */
 class DateType : public DataType
 {
 public:
@@ -20,6 +13,6 @@ public:
   virtual ~DateType() = default;
 
   int compare(const Value &left, const Value &right) const override;
-  
+
   RC to_string(const Value &val, string &result) const override;
 };
