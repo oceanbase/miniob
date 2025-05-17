589d52d4b93a3cdc62346abc39b51856dc0006cc
BaBaLiBoo <2354096@tongji.edu.cn>
2025-05-17 23:26:42 +0800

    Create drop_table_stmt.cpp

---
+++ src/observer/sql/stmt/drop_table_stmt.cpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "sql/stmt/stmt.h"
+class Db;
+class DropTableStmt : public Stmt
+{
+public:
+  DropTableStmt(const std::string &table_name) : table_name_(table_name) {}
+  virtual ~DropTableStmt() = default;
+  StmtType type() const override { return StmtType::DROP_TABLE; }
+  const std::string &table_name() const { return table_name_; }
+  static RC create(Db *db, const DropTableSqlNode &drop_table, Stmt *&stmt);
+
+private:
+  std::string table_name_;
+};
