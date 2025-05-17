0c6be60074f70fc929a333bd5539a289f0da1794
BaBaLiBoo <2354096@tongji.edu.cn>
2025-05-17 23:35:19 +0800

    Create drop_table_executor.h

---
+++ src/observer/sql/executor/drop_table_executor.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "common/rc.h"
+
+class SQLStageEvent;
+
+/**
+ * @brief 删除表的执行器
+ * 负责执行 DropTableStmt 指定的表删除操作
+ */
+class DropTableExecutor {
+public:
+  DropTableExecutor() = default;
+  virtual ~DropTableExecutor() = default;
+
+  RC execute(SQLStageEvent *sql_event);  // 执行函数声明
+};
