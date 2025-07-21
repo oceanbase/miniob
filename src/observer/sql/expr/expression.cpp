b5ccc6978b3a639c72402ddb93bf73f798510600
evtrouble <105802875+evtrouble@users.noreply.github.com>
2025-07-21 14:27:02 +0800

    Fix ArithmeticExpr.get_column in unit_test (#587)
    ### What problem were solved in this pull request?
    In unit_test, the use of empty chunks in the ArithmeticExpr.get_column
    test resulted in no calculation results. Previously, the ValueExpr was
    modified to return at least one constant value, but it was found to be
    problematic. This time, the test case will be directly modified.

--- src/observer/sql/expr/expression.cpp
+++ src/observer/sql/expr/expression.cpp
@@ -67,7 +67,7 @@ RC ValueExpr::get_value(const Tuple &tuple, Value &value) const
 
 RC ValueExpr::get_column(Chunk &chunk, Column &column)
 {
-  column.init(value_, std::max(chunk.rows(), 1));
+  column.init(value_, chunk.rows());
   return RC::SUCCESS;
 }
 
